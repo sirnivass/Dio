75f4ccd5a5cb4774870dedb9bb6c25797d66848c
sirnivass <sirnivass@gmail.com>
2023-04-10 00:00:38 -0300

    Terceira aula

--- Java/primeiro-projeto-Java/README.md
+++ Java/primeiro-projeto-Java/README.md
@@ -1,15 +1,13 @@
 Algumas observações gerais:
-
-
-
-#Convenções:
+---
+Convenções:
 
 Nomes de Classe iniciam-se sempre com letras maiúsculas.
 Toda classe precisa existir no seu diretório src do seu projeto.
 Todo arquivo é .java
 Todo .java DEVE começar com a letra maiúscula, e seus respectivos.
 A Classe deve possuir o mesmo nome do arquivo.
-
+---
 #Variáveis - camelCase
 
 Escritas com letra minúscula, mas as sucetivas com letra Maiúsculas.
@@ -18,11 +16,14 @@ Iniciam-se com letras minúsuclas
 Não podem conter espaços
 Não podem usar termos reservados
 Nome único dentro do escopo.
-
+---
+Toda variável deverá conter:
+Estrutura: Nome bem definido = (Atribuição, quando couber).
+Concatenação pode ser usaca com "concat" ou '+'
 
 ---
 Caso uma variável seja uma constante:
 1 - final - palavra reservada, define uma constante. Ex: final string BR = "Brasil"
 2 - Constante comumenta possuem todos as letras maiúsculas Ex: ESTADOS_BRASILEIROS = 27
-
+---
 
--- Java/primeiro-projeto-Java/src/MinhaClasse.java
+++ Java/primeiro-projeto-Java/src/MinhaClasse.java
@@ -5,8 +5,7 @@
  * 
  */
 
-public class MinhaClasse { //Nomes de Classe iniciam-se sempre com letras maiúsculas.
-
+public class MinhaClasse { // Nomes de Classe iniciam-se sempre com letras maiúsculas.
 
     /*
      * Nosso método main, é 'especial', por isso precisa seguir um padrão:
@@ -17,6 +16,15 @@ public class MinhaClasse { //Nomes de Classe iniciam-se sempre com letras maiús
      * string, feita por um array "[]" e nessas Strings, possuem argumentos.
      */
     public static void main(String[] args) { //main é o método principal da classe executávelSystem = Classe do sitema, possui operações de input e output.
+        
+        //Variáveis:
+        String primeiroNome = "Rodolffo", segundoNome = "Nivass", nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        int anoDeNascimento;
         System.out.print("Olá pessoas!");
+    
     }
+
+    public static String nomeCompleto(String primeiroNome String segundoNome) {
+        return primeiroNome.contains(" ").contains(segundoNome);
+    }    
 }
